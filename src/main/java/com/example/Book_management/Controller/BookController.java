26534d9d3307b589c8532c71a98eba197b4d2c50
Sakurai tomoya <onepicelove0731@gmail.com>
2025-08-09 16:47:23 +0900

    BookController作成

--- src/main/java/com/example/Book_management/Controller/BookController.java
+++ src/main/java/com/example/Book_management/Controller/BookController.java
@@ -1,5 +1,22 @@
 package com.example.Book_management.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+
+import com.example.Book_management.Service.BookService;
+
+@Controller
 public class BookController {
+    // Serviceを注入
+    @Autowired
+    private BookService bookService;
+
+    // Home画面
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
     
 }
