420fac273dfc7075d5167f30b5ca28efe789a4c8
Sakurai tomoya <onepicelove0731@gmail.com>
2025-08-09 17:55:03 +0900

    Bookコントローラ修正

--- src/main/java/com/example/Book_management/Controller/BookController.java
+++ src/main/java/com/example/Book_management/Controller/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.Book_management.Service.BookService;
 
@@ -23,5 +24,11 @@ public class BookController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    // 登録フォーム
+    @PostMapping("/register")
+    public String register() {
+        return "register";
     }
 }
