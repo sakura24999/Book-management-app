abbfd9c8ef493715346d4bd6a192bee8b4be4496
Sakurai tomoya <onepicelove0731@gmail.com>
2025-08-10 17:36:50 +0900

    UserController修正

--- src/main/java/com/example/Book_management/Controller/UserController.java
+++ src/main/java/com/example/Book_management/Controller/UserController.java
@@ -1,5 +1,13 @@
 package com.example.Book_management.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.example.Book_management.Service.UserService;
+
+@Controller
 public class UserController {
-    
+    // Serviceを注入
+    @Autowired
+    private UserService userService;
 }
