20758e1fb9946ade63338034991fee4862a04dfa
Sakurai tomoya <onepicelove0731@gmail.com>
2025-08-10 17:37:44 +0900

    UserRepository修正

--- src/main/java/com/example/Book_management/Repository/UserRepository.java
+++ src/main/java/com/example/Book_management/Repository/UserRepository.java
@@ -1,5 +1,16 @@
 package com.example.Book_management.Repository;
 
-public class UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.Book_management.Model.UserModel;
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long>{
+    // 基本検索
+    List<UserModel> findByUserId(Long userId);
+    List<UserModel> findByName(String name);
+    List<UserModel> findByEmail(String email);
+    List<UserModel> findByPassword(String password);
+    List<UserModel> findByConfirmPassword(String confirmPassword);
 }
