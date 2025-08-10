be78b33c5dc3ca9a78c0a154e97ee9eb67fcc8fc
Sakurai tomoya <onepicelove0731@gmail.com>
2025-08-10 17:39:10 +0900

    基本CRUD操作と新規登録処理追加

--- src/main/java/com/example/Book_management/Service/UserService.java
+++ src/main/java/com/example/Book_management/Service/UserService.java
@@ -1,5 +1,48 @@
 package com.example.Book_management.Service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Book_management.Model.UserModel;
+import com.example.Book_management.Repository.UserRepository;
+
+@Service
 public class UserService {
-    
+    @Autowired
+    private UserRepository userRepository;
+
+    // 基本CRUD操作
+    public UserModel create(UserModel user) {
+        return userRepository.save(user);
+    }
+
+    public UserModel findById(Long userId) {
+        return userRepository.findById(userId)
+        .orElseThrow(() -> new RuntimeException("ユーザーが見つかりません: " userId));
+    }
+
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    public UserModel update(UserModel user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+    // ユーザー登録処理
+    public UserModel register(UserModel userId) {
+        return userRepository.save(userId);
+
+        if (!userId) {
+            return "会員登録されていません。新規会員登録をしてください。";
+        }else {
+            return "会員登録されているので、ログインページからログインしてください。"
+        }
+    }
 }
