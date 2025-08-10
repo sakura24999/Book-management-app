694bff85daf61d9f74a00b37eb9ee4c321e71094
Sakurai tomoya <onepicelove0731@gmail.com>
2025-08-10 17:38:28 +0900

    基本CRUD操作追加

--- src/main/java/com/example/Book_management/Service/LoanHistoryService.java
+++ src/main/java/com/example/Book_management/Service/LoanHistoryService.java
@@ -1,5 +1,7 @@
 package com.example.Book_management.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.Book_management.Model.LoanHistoryModel;
@@ -8,5 +10,27 @@ import com.example.Book_management.Repository.LoanHistoryRepository;
 @Service
 public class LoanHistoryService {
     @Autowired
-    private LoanHistoryRepository loanhistoryrepository;
+    private LoanHistoryRepository loanhistoryRepository;
+
+    // 基本CRUD操作
+    public LoanHistoryModel create(LoanHistoryModel history) {
+        return loanhistoryRepository.save(history);
+    }
+
+    public LoanHistoryModel findById(Long historyId) {
+        return loanhistoryRepository.findById(historyId)
+        .orElseThrow(() -> new RuntimeException("貸出し履歴がありません: " historyId));
+    }
+
+    public List<LoanHistoryModel> findAll() {
+        return loanhistoryRepository.findAll();
+    }
+
+    public LoanHistoryModel update(LoanHistoryModel history) {
+        return loanhistoryRepository.save(history);
+    }
+
+    public void deleteById(Long historyId) {
+        loanhistoryRepository.deleteById(historyId);
+    }
 }
