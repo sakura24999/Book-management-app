229a77a2fb758abc80005bdd3df6bb351e8b3243
Sakurai tomoya <onepicelove0731@gmail.com>
2025-08-12 23:58:39 +0900

    LoanHistoryRepositoryの修正

--- src/main/java/com/example/Book_management/Repository/LoanHistoryRepository.java
+++ src/main/java/com/example/Book_management/Repository/LoanHistoryRepository.java
@@ -2,6 +2,8 @@ package com.example.Book_management.Repository;
 
 import org.springframework.stereotype.Repository;
 import com.example.Book_management.Model.LoanHistoryModel;
+import com.example.Book_management.Model.UserModel;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -12,4 +14,15 @@ public interface LoanHistoryRepository extends JpaRepository<LoanHistoryModel,Lo
     // 検索メソッド
     List<LoanHistoryModel>findByLendDate(LocalDateTime lendDate);
     List<LoanHistoryModel>findByReturnDate(LocalDateTime returnDate);
+
+    // 貸出中
+    List<LoanHistoryModel> findByReturnDateIsNull();
+    // 返却中
+    List<LoanHistoryModel> findByReturnDateIsNotNull();
+
+    // 外部キー関連の検索
+    // ユーザーID検索
+    List <LoanHistoryModel> findByUserUserId(Long userId);
+    // 書籍ID検索
+    List <LoanHistoryModel> findByBookBookId(Long bookId);
 }
